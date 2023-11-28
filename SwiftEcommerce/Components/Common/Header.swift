da0eaf973637c745c70efdc97a657c33372662f8
Labhansh Satpute <73733885+labhanshsatpute@users.noreply.github.com>
2023-11-29 00:02:06 +0530

    Header Done

---
+++ SwiftEcommerce/Components/Common/Header.swift
@@ -0,0 +1,45 @@
+//
+//  Header.swift
+//  SwiftEcommerce
+//
+//  Created by Labhansh Satpute on 28/11/23.
+//
+
+import SwiftUI
+
+struct Header: View {
+    
+    var title: String
+    
+    var body: some View {
+        HStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "chevron.left")
+                    .fontWeight(.medium)
+            })
+            .foregroundColor(Color.ascentDark)
+            .frame(width: 40, height: 40)
+            .background(Color.ascent.opacity(0.3))
+            .cornerRadius(10)
+            Spacer()
+            Text(title)
+                .font(.title2)
+                .fontWeight(.bold)
+                .foregroundColor(Color.ascentDark)
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "bell")
+                    .fontWeight(.medium)
+            })
+            .foregroundColor(Color.ascentDark)
+            .frame(width: 40, height: 40)
+            .background(Color.ascent.opacity(0.3))
+            .cornerRadius(10)
+
+        }
+    }
+}
+
+#Preview {
+    Header(title: "Notifications")
+}
---
+++ SwiftEcommerce/Views/NotificationView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Header(title: "Notifications")
+                Spacer()
+                VStack {
+                    
+                }
+            }.padding(15)
+        }
+    }
+}
+
+#Preview {
+    NotificationView()
+}
