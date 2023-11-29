6eb4b664a4f63e03716559ec79a23abd544e3909
Kundan2099 <121600019+Kundan2099@users.noreply.github.com>
2023-11-29 19:47:49 +0530

    account page done

--- SwiftEcommerce/Views/AccountView.swift
+++ SwiftEcommerce/Views/AccountView.swift
@@ -7,32 +7,36 @@ struct DashboardTab: View {
     var description: String
     
     var body: some View {
-        HStack(spacing: 15) {
-            Image(systemName: icon)
-                .foregroundColor(.orange)
-                .font(.system(size: 25))
-                .padding(10)
-                .background(Color.ascent.opacity(0.1))
-                .cornerRadius(50)
-            VStack(alignment: .leading) {
-                Text(label)
-                    .font(.headline)
-                    .fontWeight(.medium)
-                    .foregroundColor(Color.black.opacity(0.9))
-                    .padding(.bottom, 0.5)
-                Text(description)
-                    .font(.footnote)
-                    .foregroundColor(.gray)
-            }
-            Spacer()
-            Image(systemName: "chevron.right")
-                .foregroundColor(Color.ascent)
-            
-        }.padding(10)
+        VStack(alignment: .leading) {
+            HStack(spacing: 15) {
+                Image(systemName: icon)
+                    .foregroundColor(.orange)
+                    .font(.system(size: 25))
+                    .padding(10)
+                    .background(Color.ascent.opacity(0.1))
+                    .cornerRadius(50)
+                VStack(alignment: .leading) {
+                    Text(label)
+                        .font(.headline)
+                        .fontWeight(.medium)
+                        .foregroundColor(Color.black.opacity(0.9))
+                    Text(description)
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+                    .foregroundColor(Color.ascent)
+                
+            }.padding(.top, 10)
+        }
     }
 }
 
 struct AccountView: View {
+     
+    var email: String = "kundankapgate2005@gmail.com"
+    
     var body: some View {
         NavigationView {
             VStack(alignment: .leading) {
@@ -48,23 +52,26 @@ struct AccountView: View {
                                 .font(.title3)
                                 .fontWeight(.semibold)
                                 .foregroundColor(Color.ascentDark)
-                            Text("kundankapg2005@gmail.com")
+                            Text(email)
                                 .font(.subheadline)
                                 .foregroundColor(.gray)
                         }
                         
                     }.padding(.bottom, 10)
                     Divider()
-                    VStack{
-                        
-                         
+                    
+                    VStack(alignment: .leading) {
                             DashboardTab(icon: "shippingbox", label: "My Orders", description: "Manage your orders")
+                        
                             DashboardTab(icon: "globe", label: "Languages", description: "Manage application languages")
                             
+                            DashboardTab(icon: "questionmark.circle", label: "FAQs", description: "Manage your orders")
+                    
+                            DashboardTab(icon: "lock.shield", label: "Privacy Policy", description: "Manage application languages")
                         
-                        
-                        
-
+                            DashboardTab(icon: "gearshape", label: "All settings", description: "Manage your orders")
+                    
+                            DashboardTab(icon: "rectangle.portrait.and.arrow.right", label: "Log out", description: "Manage application languages")
                         
                     }
                 }
