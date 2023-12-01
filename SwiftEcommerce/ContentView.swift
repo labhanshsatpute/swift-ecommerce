58385f187ce08b2ee9dcb71ea73ffe8d3bf45c4a
Labhansh Satpute <73733885+labhanshsatpute@users.noreply.github.com>
2023-12-01 21:48:19 +0530

    Changes

--- SwiftEcommerce/ContentView.swift
+++ SwiftEcommerce/ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var selectedTab: String = "My Account"
+    @State var selectedTab: String = "Home"
     
     var body: some View {
         NavigationStack {
@@ -27,7 +27,7 @@ struct ContentView: View {
                 }.tag("My Account")
             }.tint(Color.ascent).frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/).padding(0)
 
-        }
+        }.navigationBarBackButtonHidden(true)
     }
 }
 
--- SwiftEcommerce/Views/GetStartedView.swift
+++ SwiftEcommerce/Views/GetStartedView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct GetStartedView: View {
     @AppStorage("currentPage") var currentPage = 1
     var body: some View {
-  
-            OnboardingScreen()
+        
+        OnboardingScreen()
     }
 }
 
@@ -21,27 +21,27 @@ struct GetStartedView: View {
 
 var totalPages = 3
 //OnBoardingScreens
-    struct OnboardingScreen: View {
-        
-        @AppStorage("currentPage") var currentPage = 1
-        
-        var body: some View {
-            ZStack{
-                if currentPage == 1{
-                    ScreenView(image: "mobile", title: "Order Food", details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry.")
-                }
-                
-                if currentPage == 2{
-                    ScreenView(image: "shopping", title: "Easy Payment", details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry.")
-                }
-                
-                if currentPage == 3{
-                    ScreenView(image: "delivery", title: "Fast Delivery", details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry.")
-                }
-              
-                }
+struct OnboardingScreen: View {
+    
+    @AppStorage("currentPage") var currentPage = 1
+    
+    var body: some View {
+        ZStack{
+            if currentPage == 1 {
+                ScreenView(image: "mobile", title: "Order Food", details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry.")
             }
+            
+            if currentPage == 2 {
+                ScreenView(image: "shopping", title: "Easy Payment", details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry.")
+            }
+            
+            if currentPage == 3 {
+                ScreenView(image: "delivery", title: "Fast Delivery", details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry.")
+            }
+            
         }
+    }
+}
 
 
 
@@ -54,98 +54,59 @@ struct ScreenView: View {
     var details: String
     
     var body: some View {
-        VStack{
-            HStack{
-                
-                if currentPage == 1{
-                    Text ("Hello !")
-                        .font(.title)
-                        .fontWeight (.semibold)
-                        .kerning (1.4)
-                }else{
-                    Button(action: {
-                        currentPage -= 1
-                    }, label: {
-                        Image(systemName: "chevron.left")
-                            .fontWeight(.bold)
-                            .font(.subheadline)
-                    }).foregroundColor(Color.ascentDark)
-
-                }
-                
-                
+        
+        NavigationStack {
+            VStack(alignment: .center) {
                 
                 Spacer ()
                 
-                Button(action: {
-                    currentPage == 3
-                }, label: {
-                    Text ("Skip")
-                        .fontWeight (.semibold)
-                        .kerning (1.2)
-                })
-            }.padding()
-                .foregroundColor(Color.ascentDark)
-            
-            
-            Spacer (minLength: 0)
-            Image (image)
-                .resizable()
-                .aspectRatio (contentMode: .fit)
-                .padding(.horizontal, 16)
-                .frame (height:300)
-            
-            Spacer (minLength: 80)
-            
-            Text (title)
-                .font(.largeTitle)
-                .fontWeight (.bold)
-                .kerning (1.2)
-                .padding (.top)
-                .padding(.bottom, 5)
-                .foregroundColor(Color.ascentDark)
-            
-            Text(details)
-                .font(.body)
-                .fontWeight (.regular)
-                .kerning (1.2)
-                .padding([.leading,.trailing])
-                .multilineTextAlignment(.center)
-            
-            Spacer(minLength: 0)
-            //                    ButtonPrimary(rightIcon: "chevron.right", text: "Next")
-            //                        .padding()
-            
-            Button(action: {
-                if currentPage <= totalPages {
-                    currentPage += 1
-                }else{
-                    currentPage = 1
-                }
-            }, label: {
+                Image (image)
+                    .resizable()
+                    .aspectRatio (contentMode: .fit)
+                    .frame (height:300)
                 
-                if currentPage == 3{
-                    Text("Get Started")
-                        .fontWeight(.semibold)
-                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,maxWidth: .infinity)
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.ascent)
-                        .cornerRadius(40)
-                        .padding(.horizontal, 16)
-                }else{
-                    Text("Next")
-                        .fontWeight(.semibold)
-                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,maxWidth: .infinity)
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.ascent)
-                        .cornerRadius(40)
-                        .padding(.horizontal, 16)
-                }
+                VStack(alignment: .center) {
+                    Text (title)
+                        .font(.largeTitle)
+                        .fontWeight (.bold)
+                        .padding(.bottom, 10)
+                        .foregroundColor(Color.ascentDark)
+                    Text(details)
+                        .font(.subheadline)
+                        .lineSpacing(5)
+                        .fontWeight (.regular)
+                        .kerning(1.05)
+                }.multilineTextAlignment(.center).padding(.bottom, 70)
                 
-            })
-            
+                VStack {
+                    if currentPage == 1 {
+                        ButtonPrimary(handleClick: {
+                            currentPage = 2
+                        }, rightIcon: "chevron.right", text: "Next")
+                    }
+                    
+                    if currentPage == 2 {
+                        ButtonPrimary(handleClick: {
+                            currentPage = 3
+                        }, rightIcon: "chevron.right", text: "Next")
+                    }
+                    
+                    if currentPage == 3 {
+                        ButtonPrimary(handleClick: {
+                            
+                        }, rightIcon: "chevron.right", text: "Next")
+                    }
+                }.padding(.bottom, 15)
+                                
+                VStack(alignment: .trailing) {
+                    NavigationLink(destination: ContentView(), label: {
+                        Text("Slip to main").fontWeight(.medium)
+                        Image(systemName: "arrow.right")
+                    }).foregroundColor(.gray)
+                }.frame(maxWidth: .infinity).multilineTextAlignment(.trailing)
+                
+            }.padding(15)
+
         }
     }
 }
