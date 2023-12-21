9673480d1960097b2096135262fe3d25b1aec343
Kundan2099 <121600019+Kundan2099@users.noreply.github.com>
2023-12-22 01:41:54 +0530

    add edit address done

--- SwiftEcommerce/Views/AddressBookView.swift
+++ SwiftEcommerce/Views/AddressBookView.swift
@@ -10,19 +10,22 @@ import SwiftUI
 struct AddressBookView: View {
     
     @Environment(\.dismiss) var dismiss
+    @State private var addAddress: Bool = false
     
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading, spacing: 20) {
                 
-                Button(action: {
-                }, label: {
-                    HStack{
-                        Image(systemName: "plus")
-                        Text("Add new address")
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                    }
+                NavigationLink(destination: AddAddress(), isActive: $addAddress) {
+                    Button(action: {
+                        addAddress = true
+                    }, label: {
+                        HStack{
+                            Image(systemName: "plus")
+                            Text("Add new address")
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                        }
                         .padding(.horizontal, 20)
                         .fontWeight(.semibold)
                         .foregroundStyle(Color.ascentDark)
@@ -30,51 +33,41 @@ struct AddressBookView: View {
                         .padding(.vertical, 14)
                         .background(Color.ascent.opacity(0.1))
                         .cornerRadius(10)
-                    
-                })
-                
+                        
+                    })
+                }
+
                 Text("Your saved addresses")
                     .font(.subheadline)
                     .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     .foregroundStyle(.gray)
                 
-                VStack(alignment: .leading) {
-                    HStack(alignment: .top) {
-                        Image(systemName: "house.fill")
-                            .font(.subheadline)
-                        VStack(alignment: .leading, spacing: 8){
-                            HStack{
-                                Text("Home").font(.headline).fontWeight(.semibold)
-                                Text("Your are here")
-                                    .font(.headline)
-                                    .foregroundStyle(Color.black.opacity(0.7))
-                                    .fontWeight(.medium)
+                NavigationLink(destination: EditAddress(), label: {
+                    VStack(alignment: .leading) {
+                        HStack(alignment: .top) {
+                            Image(systemName: "house.fill")
+                                .font(.subheadline)
+                            VStack(alignment: .leading, spacing: 8){
+                                HStack{
+                                    Text("Home").font(.headline).fontWeight(.semibold)
+                                    Text("Your are here")
+                                        .font(.headline)
+                                        .foregroundStyle(Color.black.opacity(0.7))
+                                        .fontWeight(.medium)
+                                }
+                                Text("5th Floor, Flat no 505, PR Grand Men's PG Indira Nagar, Gachibowli, Hyderabad, Telangana, India")
+                                    .font(.footnote).fontWeight(.medium)
                             }
-                            Text("5th Floor, Flat no 505, PR Grand Men's PG Indira Nagar, Gachibowli, Hyderabad, Telangana, India")
-                                .font(.footnote).fontWeight(.medium)
                         }
+                        .foregroundStyle(Color.ascentDark)
+                        .frame(maxWidth: .infinity)
+                        .padding(.horizontal, 15)
+                        .padding(.vertical, 17)
+                        .background(Color.ascent.opacity(0.1))
+                        .cornerRadius(10)
+
                     }
-                    .foregroundStyle(Color.ascentDark)
-                    .frame(maxWidth: .infinity)
-                    .padding(.horizontal, 15)
-                    .padding(.vertical, 17)
-                    .background(Color.ascent.opacity(0.1))
-                    .cornerRadius(10)
-
-                }
-                
-
-                
-//                VStack(alignment: .leading, spacing: 7) {
-//                    Text("Heather Men's Regular Fit T-Shirt").font(.subheadline).fontWeight(.semibold).lineSpacing(5)
-//                    Text("Mens Clothing").font(.caption).foregroundColor(.gray)
-//                    Text("Quantity : 3").font(.caption).foregroundColor(.gray)
-//                    HStack {
-//                        Text("$499").font(.title3).fontWeight(.semibold).lineSpacing(5).foregroundColor(Color.ascentDark)
-//                        Text("$699").font(.caption).fontWeight(.semibold).lineSpacing(5).strikethrough().foregroundColor(.gray)
-//                        Text("10% OFF").font(.caption).fontWeight(.semibold).lineSpacing(5).foregroundColor(.green)
-//                    }
-//                
+                })
                 
             Spacer()
                     
@@ -99,10 +92,9 @@ struct AddressBookView: View {
             }
     }
     
-//    @ViewBuilder
-//    func PointView(symbool: String, title: String, )
 }
 
 #Preview {
     AddressBookView()
 }
+
---
+++ SwiftEcommerce/Views/EditAddress.swift
@@ -0,0 +1,96 @@
+//
+//  EditAddress.swift
+//  SwiftEcommerce
+//
+//  Created by kundan kapgate on 22/12/23.
+//
+
+import SwiftUI
+
+struct EditAddress: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var label: String = ""
+    @State private var name: String = ""
+    @State private var email: String = ""
+    @State private var phonePrimary: String = ""
+    @State private var phoneAlternate: String = ""
+    @State private var street: String = ""
+    @State private var city: String = ""
+    @State private var postCode: String = ""
+    @State private var state = 1
+    
+
+    
+    var body: some View {
+        NavigationStack{
+            
+            ScrollView{
+                VStack{
+                   
+                    InputBox(text: $label, placeHolder: "Enter Label (eg: Home, Office)", label: "Label")
+                            .padding(.bottom, 10)
+                           
+                    InputBox(text: $name, placeHolder: "Enter Name", label: "Name")
+                        .padding(.bottom, 10)
+                        
+                    InputBox(text: $email, placeHolder: "Email Address", label: "Email Address")
+                        .padding(.bottom, 10)
+                        
+                    InputBox(text: $phonePrimary, placeHolder: "Enter Phone (10 Digits)", label: "Phone (Primary)")
+                        .padding(.bottom, 10)
+                        
+                    InputBox(text: $phoneAlternate, placeHolder: "Phone (Alternate)", label: "Phone (Alternate)")
+                        .padding(.bottom, 10)
+                    
+                    InputBox(text: $street, placeHolder: "Enter Street", label: "Street")
+                        .padding(.bottom, 10)
+                    
+                    InputBox(text: $city, placeHolder: "Enter City", label: "City")
+                        .padding(.bottom, 10)
+                    
+                    InputBox(text: $postCode, placeHolder: "Enter Postal Code (Pincode/Zipcode)", label: "Postal Code")
+                        .padding(.bottom, 10)
+                    
+                    
+                        Text("State")
+                            .font(.subheadline)
+                            .fontWeight(.medium)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading )
+                            .foregroundColor(Color.black.opacity(0.7))
+                            .padding(.bottom, 3)
+                        Picker("Select State", selection: $state){
+                            ForEach(0 ..< 25){
+                                Text("\($0) state")
+                                
+                            }
+                        }.frame(minWidth: 0, maxWidth: .infinity, alignment: .leading )
+
+
+                    }.padding()
+            }
+            
+        }.navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: { dismiss() }, label: {
+                        Image(systemName: "chevron.left")
+                            .fontWeight(.bold)
+                            .font(.subheadline)
+                    }).foregroundColor(Color.ascentDark)
+                }
+                ToolbarItem(placement: .topBarLeading) {
+                    Text("Edit Address")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.ascentDark)
+                }
+            }
+
+    }
+}
+
+#Preview {
+    EditAddress()
+}
